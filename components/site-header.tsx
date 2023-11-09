571494be6875e3d5b568873e629d023b376ea8a4
MicKoreaYoutube <118183460+MicKoreaYoutube@users.noreply.github.com>
2023-11-09 08:34:10 +0000

    update header

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -14,14 +14,6 @@ export function SiteHeader() {
       <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
         <MainNav items={siteConfig.mainNav} />
         <div className="flex flex-1 items-center justify-end space-x-4">
-        <Link
-              href={siteConfig.links.shadcnuiDocs}
-              target="_blank"
-              rel="noreferrer"
-              className={buttonVariants()}
-            >
-              <LogIn />
-            </Link>
         </div>
       </div>
     </header>
