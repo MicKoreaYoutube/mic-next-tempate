cd88e0bdd238d41fa70481c056285640b2625066
MicKorea Youtube <mickoreayt@gmail.com>
2023-11-18 04:49:28 +0000

    와 ㅅㅂ 성공!

--- app/docs/[subject]/[title]/page.tsx
+++ app/docs/[subject]/[title]/page.tsx
@@ -1,15 +1,21 @@
-import { siteConfig } from "@/config/site";
+'use client'
 
-import parse from "html-react-parser";
-import { notFound } from "next/navigation";
+import { siteConfig } from "@/config/site"
+
+import { docsSidebarItem } from "@/types/sidebar"
+
+import parse from "html-react-parser"
+import { notFound } from "next/navigation"
+import { useEffect } from "react"
 
 export default function DocsSubjectPage({ params }: { params: { subject: string, title: string } }) {
-    const foundSubjectObject = siteConfig.docsSidebarContent.find(obj => obj.title == decodeURI(params.subject));
-    const foundObject = foundSubjectObject?.content?.find(obj => obj.title == decodeURI(params.title));
 
-    return (
-        <>
-            {foundObject?.doc ? parse(foundObject?.doc) : notFound()}
-        </>
-    );
+  const foundSubject = siteConfig.docsSidebarContent.find(obj => obj.title == decodeURI(params.subject))
+  const foundObject: { title: string; doc?: string } | undefined = foundSubject?.content?.find(obj => obj.title == decodeURI(params.title))
+
+  return (
+    <>
+      {foundObject?.doc ? parse(foundObject?.doc) : notFound()}
+    </>
+  );
 }
